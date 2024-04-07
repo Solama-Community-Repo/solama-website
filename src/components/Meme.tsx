eb8af888a88d32245bebbfdc520fcf6be9d19cd0
kcrehberg <krehberg12@gmail.com>
2024-04-06 21:11:13 -0400

    improve meme images scroll

--- src/components/Meme.tsx
+++ src/components/Meme.tsx
@@ -1,20 +1,21 @@
 import Image from "next/image";
 import { images } from "../utils/constants/constants";
+import Marquee from "react-fast-marquee";
 
 const Meme = () => {
   return (
     <section id="meme" className="mt-32">
-      <div className="relative h-[35px] w-[335px] mx-auto xl:h-[59px] xl:w-[560px] ">
+      <div className="relative mx-auto h-[35px] w-[335px] xl:h-[59px] xl:w-[560px] ">
         <Image src="/solama_meme.png" alt="Solama Meme" layout="fill" />
       </div>
-      <div className="group overflow-hidden whitespace-nowrap">
-        <div className="animate-slide mt-10 inline-block group-hover:[animation-play-state:paused]">
+      <div className="mt-10">
+        <Marquee pauseOnHover speed={150}>
           {images.map((img) => (
             <div key={img.alt} className="mx-10 inline-block">
               <Image src={img.src} alt={img.alt} height={300} width={300} priority />
             </div>
           ))}
-        </div>
+        </Marquee>
       </div>
     </section>
   );
