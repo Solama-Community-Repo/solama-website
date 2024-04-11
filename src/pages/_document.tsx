1879dffc5d1e260e82b35fef44bf155dcc669210
kcrehberg <krehberg12@gmail.com>
2024-04-10 21:13:11 -0400

    add favicon and seo stuff

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,24 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <meta
+          name="description"
+          content="Solama the Official 'Unofficial' Solana Mascot"
+          key="desc"
+        />
+        <link rel="icon" href="/solama_logo.png" />
+        <meta name="keywords" content="web3, meme, solana, blockchain, defi, crypto, cryptocurrency, memecoins" />
+        <meta property="og:image" content="https://www.solama.com/solama_logo.png" />
+        <meta name="twitter:image" content=" https://www.solama.com/solama_logo.png" />
+        <meta
+          property="og:description"
+          content="Solama the Official 'Unofficial' Solana Mascot"
+        />
+        <meta
+          name="twitter:description"
+          content="Solama the Official 'Unofficial' Solana Mascot"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <script src="https://terminal.jup.ag/main-v2.js" data-preload />
         <script type="text/javascript" src="https://files.coinmarketcap.com/static/widget/currency.js"></script>
       </Head>
@@ -11,8 +29,8 @@ export default function Document() {
         <Main />
         <NextScript />
         <div
-		  id="coinmarketcap-currency-widget"
-          className="hidden coinmarketcap-currency-widget"
+          id="coinmarketcap-currency-widget"
+          className="coinmarketcap-currency-widget hidden"
           data-currencyid="29015"
           data-base="USD"
           data-secondary=""
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,19 @@
 import React from "react";
+import Head from "next/head";
 import Layout from "../components/Layout";
 import Home from "../components/Home";
 
 const IndexPage = ({}) => {
-    return (
-        <Layout>
-            <Home />
-        </Layout>
-    );
+  return (
+    <>
+      <Head>
+        <title>SOLAMA</title>
+      </Head>
+      <Layout>
+        <Home />
+      </Layout>
+    </>
+  );
 };
 
 export default IndexPage;
