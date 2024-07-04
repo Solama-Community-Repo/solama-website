29ef88ed21af14feef8638a36abc9c7b4eceac64
rohkyN <164794953+rohkyN@users.noreply.github.com>
2024-07-04 03:03:18 +0200

    Update Home.tsx
    deleted game and change trendz to task bot

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,7 +3,7 @@ import Buy from "./Buy";
 import Game from "./Game";
 import Meme from "./Meme";
 import Tokenomics from "./Tokenomics";
-import Trendz from "./Trendz";
+import Task Bot from "./Task Bot";
 import Welcome from "./Welcome";
 import Swap from "./Swap";
 import Partners from "./Partners";
@@ -21,8 +21,8 @@ const Home = () => {
       <Buy />
 	  <Team />
 	  <Meme />
-      <Trendz />
-      <Game />
+      <Task Bot />
+     
     </div>
   );
 };
